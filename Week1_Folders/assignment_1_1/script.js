fd48589546110d3716edbccb64bebe5bb9454b35
book81able <ecobb81@gmail.com>
2018-01-19 01:07:45 -0500

    Leavey is here
    WOop

--- Week1_Folders/assignment_1_1/script.js
+++ Week1_Folders/assignment_1_1/script.js
@@ -1,6 +1,23 @@
+var grassX;
+var grassY;
 
 function setup(){
-  createCanvas(500,500);
-  fill(200,0,0);
+	createCanvas(500,500);
+	fill(0,200,100);
+	rect(0,250,500,250);
+	fill(214, 255, 252);
+	rect(0,0,500,250);
+}
 
+function draw(){
+	fill(0,200,0)
+	for (var i = 0; i <= 10; i++) {
+		grassX = random(0,500);
+		grassY = random(250,500);
+		for (var i = 0; i <= 10; i++) {
+			bezier(grassX,grassY,grassX,grassY-10,
+				   grassX+random(-15,15),grassY-random(0,30),
+				    grassX+random(-15,15),grassY-random(0,30)); 
+		}
+	}
 }
