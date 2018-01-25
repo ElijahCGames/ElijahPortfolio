9577461a477b602e205b619162ae257719cd097a
book81able <ecobb81@gmail.com>
2018-01-24 19:58:03 -0500

    LandscapeFinalThings
    Yes

--- Week1_Folders/assignment_1_1/script.js
+++ Week1_Folders/assignment_1_1/script.js
@@ -4,16 +4,19 @@ var rockX;
 var rockY;
 var cloudX;
 var cloudY;
-var cloudRadius
+var cloudRadius;
+var brackX;
+var branchY;
+var branchDiam;
 function setup(){
 	createCanvas(500,500);
 	fill(0,200,100);
 	rect(0,250,500,250);
 	fill(214, 255, 252);
 	rect(0,0,500,250);
-	fill(200,0,0)
-	fill(0,200,0)
-	for (var i = 0; i <= 20; i++) {
+	fill(200,0,0);
+	fill(0,200,0);
+	for (var i = 0; i <= 40; i++) {
 		grassX = random(0,500);
 		grassY = random(250,500);
 		for (var e = 0; e <= 10; e++) {
@@ -35,11 +38,33 @@ function setup(){
 	}
 	for (var i = 0; i <= 30; i++) {
 		noStroke();
-		fill(255,100)
+		fill(255,150)
 		cloudX = random(50,450);
 		cloudY = random(50,200);
 		cloudRadius = random(20,50);
 		ellipse(cloudX,cloudY,cloudRadius);
+		ellipse(cloudX-20,cloudY+(cloudRadius/4),cloudRadius/1.5);
+		ellipse(cloudX+20,cloudY+(cloudRadius/4),cloudRadius/1.5);
+	}
+	fill(127, 91, 53);
+	quad(100,400,130,400,140,150,90,150);
+	for (var i = 0; i <= 50; i++) {
+		branchX = random(30,200);
+		branchY = random(100,200);
+		branchDiam = random(10,60);
+		branchRad = branchDiam/2;
+		fill(0,250,0);
+		ellipse(branchX,branchY,branchDiam);
+		fill(0,100,0);
+		beginShape();
+		vertex(branchX + branchRad, branchY);
+		bezierVertex(branchX + branchRad, branchY+branchRad*1.5, 
+					 branchX - branchRad, branchY+branchRad*1.5,
+					 branchX - branchRad, branchY);
+		bezierVertex(branchX + branchRad, branchY+branchRad-5, 
+					 branchX - branchRad, branchY+branchRad-5,
+					 branchX + branchRad, branchY);
+		endShape();
 	}
 }
 
