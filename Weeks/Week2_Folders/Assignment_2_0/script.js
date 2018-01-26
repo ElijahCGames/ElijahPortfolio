143c0a21f989eb975fbc364fea32bffd64342126
book81able <ecobb81@gmail.com>
2018-01-25 20:52:39 -0500

    Even Beliven
    Woah

--- Weeks/Week2_Folders/Assignment_2_0/script.js
+++ Weeks/Week2_Folders/Assignment_2_0/script.js
@@ -1,3 +1,6 @@
+var div = 10;
+var divider = 6.5;
+var rad = div/divider;
 
 function setup(){
   var can = createCanvas(600,600);
@@ -7,18 +10,18 @@ function setup(){
 
 function draw(){
   noStroke();
-  for (var x = 0; x < width; x+=width/10) {
-    for(var y = 0; y <height; y+=height/10) {
-      if(x/(width/10)%2 == 0 && y/(height/10)%2==0){
-        fill(255);
-      }else if(x/(width/10)%2==0){
+  for (var x = 0; x < width; x+=width/div) {
+    for(var y = 0; y <height; y+=height/div) {
+      if(x/(width/div)%2 == 0 && y/(height/div)%2==0){
+        fill((255/width)*x,0,(255/height)*y);
+      }else if(x/(width/div)%2==0){
         fill(0,(255/width)*x,0)
-      }else if(y/(height/10)%2==0){
-        fill(0,0,(255/height)*y)
+      }else if(y/(height/div)%2==0){
+        fill((255/height)*y,0,0)
       }else{
         fill(0,(255/width)*x,(255/height)*y);
       }
-      ellipse(x+width/20,y+height/20,min(width/20,height/20));
+      ellipse(x+width/div,y+height/div,min(width/rad,height/rad));
     }
   }
 }
