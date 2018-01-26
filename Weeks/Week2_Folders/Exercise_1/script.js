8ad5eea12f959c38a328a69c41e0f9229b3faa3e
book81able <ecobb81@gmail.com>
2018-01-25 20:34:29 -0500

    Assignment 2
    Simple

--- Weeks/Week2_Folders/Exercise_1/script.js
+++ Weeks/Week2_Folders/Exercise_1/script.js
@@ -7,17 +7,9 @@ function setup(){
 
 function draw(){
   noStroke();
+  fill(0,200,200);
   for (var x = 0; x < width; x+=width/10) {
     for(var y = 0; y <height; y+=height/10) {
-      if(x/(width/10)%2 == 0 && y/(height/10)%2==0){
-        fill(255);
-      }else if(x/(width/10)%2==0){
-        fill(0,(255/width)*x,0)
-      }else if(y/(height/10)%2==0){
-        fill(0,0,(255/height)*y)
-      }else{
-        fill(0,(255/width)*x,(255/height)*y);
-      }
       ellipse(x+width/20,y+height/20,min(width/20,height/20));
     }
   }
