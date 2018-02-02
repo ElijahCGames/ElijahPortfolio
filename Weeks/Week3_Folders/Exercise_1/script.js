505f07fb723408497aa8ea16444d5b57dbb2b157
book81able <ecobb81@gmail.com>
2018-02-01 21:14:15 -0500

    Return to the program I used to know

--- Weeks/Week3_Folders/Exercise_1/script.js
+++ Weeks/Week3_Folders/Exercise_1/script.js
@@ -9,8 +9,8 @@ var ySpeed;
 var img;
 // declare a var for image width, assign 40 
 // declare a var for image height, assign 30
-var imageWidth = 800;
-var imageHeight = 800;
+var imageWidth = 40;
+var imageHeight = 30;
 // use the preload()function to load an image, 
 // format: img = loadImage('assets/imageName.png');
 // you'll need to create a folder called assets, and include the png there
@@ -22,8 +22,8 @@ function setup() {
   img = loadImage("../../../Images/B&Wback.png");
   // set xPos to be half of the width 
   // set yPos to be half of the height
-  xPos = width/2-3;
-  yPos = height/2-4
+  xPos = width/2;
+  yPos = height/2
   // assign xSpeed and ySpeed 
   // with random values between 1 and 10
   xSpeed = random(1,10);
@@ -43,10 +43,10 @@ function draw(){
   ySpeed += yAccel;
   // if we reach the edge of the canvas
   // turn around (toggle xSpeed negative to positive)
-  if(xPos<0-imageWidth/2|| xPos>width-imageWidth/2){
+  if(xPos<0|| xPos>width-imageWidth){
     xSpeed *= -1;
   }
-  if(yPos<0-imageHeight/2|| yPos>height-imageHeight/2){
+  if(yPos<0 || yPos>height-imageHeight){
     ySpeed *= -1;
   }
   // same as above, toggle ySpeed if we hit the top or bottom
