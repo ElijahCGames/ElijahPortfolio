b61538c3ad377c7a633ab4e21399d9b2633d0b72
book81able <ecobb81@gmail.com>
2018-02-22 17:34:12 -0500

    CURLING EXTREME SUPER DELUXE EDITION HORSE PUNCHER!
    (No horses were punced in the making of this game)
    
    (Super stands for bugs) There's many of them)

--- Weeks/Week5_Folders/Assignment_5_0/script.js
+++ Weeks/Week5_Folders/Assignment_5_0/script.js
@@ -3,23 +3,72 @@ var ston = [];
 var leng = 0;
 //Game State Set
 var gameState = "start";
+var gameMode = "nothing"
+var gameModes = ["classic","target","party"];
 //Stone in Play
 var aimingStone;
 //Target Set
-var xyTarget = [];
+var targ;
 //Scores
 var scoreValue = 0;
 var highScore = 0;
+//Powerups
+var pUp = [];
+var types = [
+	small = {
+		color:[0,200,0],
+		id:0
+	},
+	fast = {
+		color:[157, 243, 14],
+		id:1
+	},
+	slip = {
+		color:[0,200,200],
+		id:2
+	},
+	mass = {
+		color:[56, 12, 10],
+		id:3
+	},
+	curly = {
+		color:[244, 66, 209],
+		id:4
+	},
+	sup = {
+		color:[200,0,0],
+		id:5
+	}
+];
+//Curling Counting
+redWins = 0;
+yellowWins = 0;
+lastWin = 0;
+wins = [redWins,yellowWins];
+colors = [[196, 15, 15],[247, 243, 14]];
 //Round set
+var throws = 0;
+var throwNum = 0;
+var targetNum = 5;
+var classicNum = 12;
+var partyNum = 8;
+//End Set
 var ends = 0;
-
+var endNum = 6;
+var partyEnds = 2;
+//Somethings
 var inMotion = 0;
+//Aesthtics
+var font;
+
+
 
 function setup() {
   can = createCanvas(200,600);
   can.parent("sketch-holder");
   angleMode(DEGREES);
   rectMode(CENTER);
+  textFont('Abril Fatface');
 }
 
 function draw(){
@@ -33,23 +82,21 @@ function draw(){
   	curling();
   }else if(gameState == "sliding"){
   	sliding();
+  }else if(gameState == "interval"){
+  	interval();
   }
-  if(gameState != "start"){
-  	target();
-  	text("Score: " + scoreValue + " Ends: " + ends, 5, 10);
-  	text("High Score: " + highScore , 5, 20);
+  if(gameState != "start" && gameState != "interval"){
+  	UI();
+  	if(gameMode == "party"){
+  		powerUpManager();
+  	}
   }
 }
 
 //To Start the Game
 function keyReleased(){
-	if(gameState == "start" && keyCode == 32){
-		gameState = "aiming";
-		var firstStone = new Stone();
-		ston.push(firstStone);
-		xyTarget = [random(10,200),random(10,300)];
-		print(highScore);
-		return false;
+	if(gameMode == "sliding" && keyCode == 81){
+		nextStone();
 	}
 }
 
@@ -62,6 +109,42 @@ function mouseReleased(){
 	if(gameState == "aiming" && mouseY<450){
 		aimingStone.speedSet();
 		gameState = "curling";
+	}
+	if(gameState == "start"){
+		for(var i=0;i<3;i++){
+			if(pressed(i)){
+				gameState = "aiming";
+				gameMode = gameModes[i];
+				targ = new Target();
+				var firstStone = new Stone();
+				ston.push(firstStone);
+				if(gameMode == "classic"){
+					yellowWins = 0;
+					redWins = 0;
+					throwNum = classicNum;
+				}
+				if(gameMode == "party"){
+					throwNum = partyNum;
+					endNum = partyEnds;
+					yellowWins = 0;
+					redWins = 0;
+					var newPower = new Powerup();
+					pUp.push(newPower);
+				}
+			}
+		}
+	}
+	if(gameState == "interval"){
+		if(pressed(2)){
+			gameState = "aiming";
+			var firstStone = new Stone();
+			ston.push(firstStone);
+			if(gameMode == "party"){
+				pUp = [];
+				var newPower = new Powerup();
+				pUp.push(newPower);
+			}
+		}
 	}
 }
 
@@ -80,17 +163,91 @@ function groundElements(){
 	line(0,450,width,450);
 }
 
+function UI(){
+	if(gameMode == "target"){
+		throwNum = targetNum;
+	  	targ.display();
+	  	text("Score: " + scoreValue + " Throws: " + throws, 5, 10);
+	  	text("High Score: " + highScore , 5, 20);
+  	}
+  	if(gameMode == "classic" || gameMode == "party"){
+  		for(var i=0; i<throwNum-throws;i++){
+  			if(i%2 == 1){
+  				fill(colors[lastWin]);
+  				ellipse(20,600-i*10,15);
+  			}else{
+  				fill(colors[lastLose()])
+  				ellipse(width-20,590-i*10,15);
+  			}
+  		}
+  		fill(colors[lastWin]);
+  		text(wins[lastWin],20,470);
+  		fill(colors[lastLose()]);
+  		text(wins[lastLose()],width-20,470);
+  	}
+}
+
+function powerUpManager(){
+	for (var i = pUp.length - 1; i >= 0; i--) {
+		pUp[i].display();
+		pUp[i].detect(i);
+	}
+}
 //When GameState = start
 function startScreen(){
 	fill(0);
 	noStroke();
 	textAlign(CENTER);
-  text("Press Space to Start",width/2,300);
-  text("High Score: " + highScore,width/2,320);
-  text("Click where you want the stone to go",width/2,340);
-  text("USe A and D to add curl to the stone",width/2,360);
+	if(gameMode == "target"){
+		 text("High Score: " + highScore,width/2,260);
+	}
+	if(gameMode == 'classic'){
+		if(redWins>yellowWins){
+			text("RED WINS",width/2,240);
+		}else if(yellowWins>redWins){
+			text("YELLOW WINS",width/2,240);
+		}else if(redWins == yellowWins){
+			text("YOU TIED",width/2,200);
+			text("I'm too lazy to implment an",width/2,220);
+			text("extra end so just play again!",width/2,240);
+		}
+		text("Red Points: " + redWins + " Yellow Points: " + yellowWins ,width/2,260);
+	}
+  text("Click where you want the stone to go",width/2,280);
+  text("Use A and D to add curl to the stone",width/2,300);
+  hover(0);
+  fill(244, 241, 66);
+  rect(width/2,320,150,20);
+  hover(1);
+  fill(239, 65, 52);
+  rect(width/2,350,150,20);
+  hover(2);
+  fill(51, 108, 239);
+  rect(width/2,380,150,20);
+  fill(0);
+  noStroke();
+  text("Classic Mode (2p)",width/2,325);
+  text("Target Mode",width/2,355);
+  text("Party Mode",width/2,385);
   ston = [];
+  pUp = [];
   leng = 0;
+}
+
+function hover(button){
+	if(pressed(button)){
+  		noStroke();
+  }else{
+  	stroke(0);
+  }
+}
+
+function pressed(button){
+	if(mouseX>(width/2)-75 && mouseX<(width/2)+75 && mouseY>310+30*button && mouseY<330+30*button){
+		return true;
+	}else{
+		return false;
+	}
 }
 
 function aiming(){
@@ -109,12 +266,12 @@ function curling(){
 }
 
 function sliding(){
-	aimingStone.move();
+	aimingStone.move(leng);
 	for(var i = 0; i<=leng; i++){
 	  	ston[i].display(i);
 	  	ston[i].collide();
 	  	if(ston[i].collided){
-	  		ston[i].bounceMove();
+	  		ston[i].bounceMove(i);
 	  	}
 	  	if(ston[i].justCollided){
 	  		var currentFrame = frameCount
@@ -125,47 +282,152 @@ function sliding(){
 	}
 }
 
-//Target Setup
-function target(){
+function interval(){
+	background(colors[lastWin]);
+	groundElements();
 	fill(0);
-	rect(xyTarget[0],xyTarget[1],20,5);
-	rect(xyTarget[0],xyTarget[1],5,20);
 	noStroke();
-	textAlign(LEFT);
+	text("End #" + ends,width/2,height/2+20);
+  	text("Red: " + redWins + " Yellow: " + yellowWins,width/2,height/2+40);
+	if(lastWin == 0){
+  		text("Red Wins",width/2,height/2);
+  		text("Yellow has the Hammer",width/2,height/2+60);
+	}else{
+		text("Yellow Wins",width/2,height/2);
+		text("Red has the Hammer",width/2,height/2+60);
+	}
+	hover(2);
+	fill(51, 108, 239);
+  	rect(width/2,380,150,20);
+  	noStroke();
+  	fill(0);
+  	text("Next End",width/2,385);
+  	ston = [];
+  	leng = 0;
 }
-
 //Calculates Scores
 function scoring(){
-	var long = abs(xyTarget[0]-aimingStone.x);
-	var tall = abs(xyTarget[1]-aimingStone.y);
-	var protoScore = sqrt(sq(long)+sq(tall));
+	var protoScore = dist(targ.x,targ.y,aimingStone.x,aimingStone.y);
 	var refinedScore = round(map(protoScore,0,100,10000,0));
 	if(refinedScore<0){
 		refinedScore = 0;
 	}
 	scoreValue += refinedScore;
-	xyTarget = [random(10,200),random(10,300)];
+	targ = new Target();
+	if(highScore<scoreValue){
+			highScore = scoreValue;
+	}
 }
 
+function tallying(){
+	var tallyHelper = [];
+	for(var i = 0; i<=leng; i++){
+		var distance = dist(ston[i].x,ston[i].y,width/2,100);
+		ston[i].dist = round(distance);
+		tallyHelper.push(round(distance));
+	}
+	sort(tallyHelper);
+	for(var i = 0; i<=leng; i++){
+		if(ston[i].dist == tallyHelper[0]){
+			if(lastWin==0){
+				if(i%2 == 0){
+					redWins++;
+					checkNext(tallyHelper,"Red",1);
+					lastWin = 0
+				}else{
+					yellowWins++
+					checkNext(tallyHelper,"Yellow",1);
+					lastWin = 1
+				}
+			}else{
+				if(i%2 == 0){
+					yellowWins++;
+					checkNext(tallyHelper,"Yellow",1);
+					lastWin = 1
+				}else{
+					redWins++;
+					checkNext(tallyHelper,"Red",1);
+					lastWin = 0
+				}
+			}
+		}
+	}
+	print(redWins + ", " + yellowWins);
+	print(lastWin);
+}
+
+function lastLose(){
+	return(abs(lastWin-1));
+}
+
+function checkNext(array,winner,place){
+	for(var i = 0; i<=leng; i++){
+		if(ston[i].dist == array[place]){
+			if(winner == "Red"){
+				if(lastWin==0){
+					if(i%2 == 0){
+						redWins++
+						place++
+						checkNext(array,"Red",place);
+					}
+				}else{
+					if(i%2==1){
+						redWins++
+						place++
+						checkNext(array,"Red",place);
+					}
+				}
+			}else{
+				if(lastWin==0){
+					if(i%2 == 1){
+						yellowWins++
+						place++
+						checkNext(array,"Yellow",place);
+					}
+				}else{
+					if(i%2==0){
+						yellowWins++
+						place++
+						checkNext(array,"Yellow",place);
+					}
+				}
+			}
+		}
+	}
+}
 //Reset the loop
 function nextStone(){
-	scoring();
+	if(gameMode == "target"){
+		scoring();
+	}
 	for(s of ston){
 		s.justCollided = false;
 	}
-	if(ends >= 10){
-		if(highScore<scoreValue){
-			highScore = scoreValue;
-		}
-		ends = 0;
-		scoreValue = 0;
+	if(throws >= throwNum-1){
+		if(gameMode == "classic" || gameMode == "party"){
+			tallying();
+			if(ends>=endNum-1){
+				gameState = "start"
+				ends = 0;
+			}else{
+				gameState = "interval"
+				ends++;
+			}
+		}else{
 		gameState = "start"
+		}
+		throws = 0;
+		scoreValue = 0;
 	}else{
 		let newStone = new Stone();
 		ston.push(newStone);
 		leng++;
 		aimingStone = ston[leng];
-		ends++;
+		throws++;
+		if(gameMode == "party"){
+			var newPower = new Powerup();
+			pUp.push(newPower);
+		}
 		gameState = "aiming";
 	}
 }
@@ -183,6 +445,9 @@ class Stone{
 		this.roateSpeed = 0;
 		this.collided = false;
 		this.justCollided = false;
+		this.dist = 0;
+		this.diameter = 30;
+		this.mass = 30;
 	}
 	//Show the Stones
 	display(colorSet){
@@ -190,31 +455,28 @@ class Stone{
 			this.rotateSpeed = -this.curlRate*30
 		}
 		noStroke();
-		if(colorSet%2 ==0){
-			fill(196, 15, 15);
+		if(colorSet%2 == 0){
+			fill(colors[lastWin]);
 		}else{
-			fill(247, 243, 14);
+			fill(colors[lastLose()]);
 		}
 		translate(this.x,this.y);
-		ellipse(0,0,30);
+		ellipse(0,0,this.diameter);
 		fill(200);
 		rotate(this.rotateSpeed);
-		rect(0,0,4,20);
+		rect(0,0,4,this.diameter*(2/3));
 		rotate(-this.rotateSpeed);
 		translate(-this.x,-this.y);
 	}
 	//Move the Stones after they cross the hog line
-	move(){
+	move(stoneNum){
 		this.y += this.ySpeed;
 		this.x += this.xSpeed;
 		this.xSpeed += this.curlRate/10000;
 		this.ySpeed *= this.friction;
 		this.xSpeed *= this.friction;
 		this.curlRate*=this.friction;
-		if(this.x<15 || this.x>width-15){
-			this.xSpeed = -this.xSpeed;
-			this.justCollided = false;
-		}
+		this.wallBounce(leng);
 		if(this.xSpeed<.05 && this.xSpeed>-.05 && this.curlRate<2.5 && this.curlRate> -2.5){
 			this.xSpeed = 0;
 		}
@@ -253,7 +515,7 @@ class Stone{
 	collide(){
 		for (var i = ston.length - 1; i >= 0; i--) {
 			if(ston[i]!== this){
-				if(dist(this.x,this.y,ston[i].x,ston[i].y)<30){
+				if(dist(this.x,this.y,ston[i].x,ston[i].y)<(this.diameter/2+ston[i].diameter/2)){
 					if(this.ySpeed< -.1 || this.ySpeed>.1){
 						if(!this.justCollided || !ston[i].justCollided){
 						//Saving the speed of the slower stone, in case something happens
@@ -262,11 +524,11 @@ class Stone{
 						//The Dot Products of things
 						var multiston = ((((saveX-this.xSpeed)*(ston[i].x-this.x))+((saveY-this.ySpeed)*(ston[i].y-this.y)))/(sq(ston[i].x-this.x)+sq(ston[i].y-this.y)));
 						//Setting the staionary stone speed
-						ston[i].ySpeed = ston[i].ySpeed - (multiston*(ston[i].y-this.y));
-						ston[i].xSpeed = ston[i].xSpeed - (multiston*(ston[i].x-this.x));
+						ston[i].ySpeed = ston[i].ySpeed - (((2*this.mass)/(this.mass+ston[i].mass))*multiston*(ston[i].y-this.y));
+						ston[i].xSpeed = ston[i].xSpeed - (((2*this.mass)/(this.mass+ston[i].mass))*multiston*(ston[i].x-this.x));
 						//Setting the moving stone Speed
-						this.ySpeed = this.ySpeed - (multiston*(this.y-ston[i].y));
-						this.xSpeed = this.xSpeed - (multiston*(this.x-ston[i].x));
+						this.ySpeed = this.ySpeed - (((ston[i].mass)/(this.mass+ston[i].mass))*multiston*(this.y-ston[i].y));
+						this.xSpeed = this.xSpeed - (((ston[i].mass)/(this.mass+ston[i].mass))*multiston*(this.x-ston[i].x));
 						//Telling the world about the collision
 						if(ston[i].collided == false){
 							inMotion++;
@@ -280,16 +542,13 @@ class Stone{
 			}
 		}
 	}
-	bounceMove(){
+	bounceMove(stoneNum){
 		if(aimingStone != this){
 			this.y += this.ySpeed;
 			this.x += this.xSpeed;
 			this.ySpeed *= this.friction;
 			this.xSpeed *= this.friction;
-			if(this.x<15 || this.x>width-15){
-				this.xSpeed = -this.xSpeed;
-				this.justCollided = false;
-			}
+			this.wallBounce(stoneNum);
 			if(this.ySpeed>-.1 && this.ySpeed<.1){
 				this.ySpeed = 0;
 			}
@@ -302,26 +561,131 @@ class Stone{
 			}
 		}
     }
+    wallBounce(stoneNum){
+    	if(gameMode != "classic"){
+    		if(this.x<this.diameter/2 || this.x>width-this.diameter/2){
+				this.xSpeed = -this.xSpeed;
+				this.justCollided = false;
+			}
+		}
+		if(gameMode == "classic"){
+			if(this.x<0 || this.x>width || this.y<0){
+				ston.splice(stoneNum,1,new BadMove());
+				nextStone();
+			}
+		}
+		if(gameMode == "party"){
+			if(this.y<this.diameter/2 || this.y>451){
+				this.ySpeed = -this.ySpeed;
+				this.justCollided = false;
+			}
+		}
+    }
 	speedSet(){
 		this.xSpeed = map(mouseX,0,width,-.7,.7);
 		this.ySpeed = map(mouseY,0,height,-3.44,.4)
 	}
 
 	spinDirection(x,y,theta){
-	if(theta>0){
-		for(var i = 0; i<=theta;i++){
-			var xEnd = 50*cos(90-i) + x;
-			var yEnd = y-50*sin(90-i);
-			stroke(0,200,0);
-			line(x,y,xEnd,yEnd);
-		}
-	}else if(theta<0){
-		for(var i = 0;i>=theta;i--){
-			var xEnd = 50*cos(90-i) + x;
-			var yEnd = y-50*sin(90-i);
-			stroke(0,200,0);
-			line(x,y,xEnd,yEnd);
+		if(theta>0){
+			for(var i = 0; i<=theta;i++){
+				var xEnd = 50*cos(90-i) + x;
+				var yEnd = y-50*sin(90-i);
+				stroke(0,200,0);
+				line(x,y,xEnd,yEnd);
+			}
+		}else if(theta<0){
+			for(var i = 0;i>=theta;i--){
+				var xEnd = 50*cos(90-i) + x;
+				var yEnd = y-50*sin(90-i);
+				stroke(0,200,0);
+				line(x,y,xEnd,yEnd);
+			}
 		}
 	}
 }
+class Target{
+	constructor(){
+		this.x = random(10,200);
+		this.y = random(10,300);
+	}
+	display(){
+		fill(0);
+		rect(this.x,this.y,20,5);
+		rect(this.x,this.y,5,20);
+		noStroke();
+		textAlign(LEFT);
+}
+}
+class Powerup{
+	constructor(){
+		this.x = random(15,width-15);
+		this.y = random(200,400);
+		this.type = random(types);
+		this.diam = 30
+	}
+	display(){
+		fill(this.type.color);
+		ellipse(this.x,this.y,this.diam);
+	}
+	detect(index){
+		var stoneDistance = dist(aimingStone.x,aimingStone.y,this.x,this.y)
+		if(stoneDistance<aimingStone.diameter/2+this.diam/2){
+			this.diam = stoneDistance;
+			if(stoneDistance<aimingStone.diameter/2){
+				this.power();
+				pUp.splice(index,1)
+			}
+		}
+	}
+	power(){
+		if(this.type.id == 0){
+			aimingStone.diameter /= 2;
+			aimingStone.mass /= 2;
+		}else if(this.type.id == 1){
+			aimingStone.xSpeed *= 5;
+			aimingStone.ySpeed *= 5;
+		}else if(this.type.id == 2){
+			var tiny = 1 - aimingStone.friction;
+			aimingStone.friction += tiny/2;
+			print(aimingStone.friction)
+		}else if(this.type.id == 3){
+			aimingStone.mass *= 4;
+			aimingStone.diameter *= 1.2;
+		}else if(this.type.id == 4){
+			aimingStone.curlRate *= 10;
+		}else if(this.type.id == 5){
+			if(this.x<30){
+				this.x=30;
+			}
+			if(this.x>width-30){
+				this.x = width-30;
+			}
+			aimingStone.mass *=10;
+			aimingStone.diameter *= 1.5;
+			aimingStone.xSpeed *= 4
+			aimingStone.ySpeed *= 4
+			aimingStone.culrRate *=10;
+		}
+	}
+}
+
+//I love this. It's a botched way to make it so stones that leave the area aren't counted but everything else works
+//It's great
+//I love it
+//It will cause NOOOOOOOOO bugs
+//Yeah
+//No Bugs
+//Or an invertabrates for that matter.
+class BadMove{
+	constructor(){
+		this.x = 10000;
+		this.y = 10000;
+	}
+	display(){
+
+	}
+	collide(){
+
+	}
 }
